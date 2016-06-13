d64413b26ce5146846d6575245856f1dc010c8d9
diggzhang <diggzhang@gmail.com>
2016-06-13 11:29:42 +0800

    add test case

---
+++ test/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const koa = require('koa');
+const app = module.exports = koa();
+const bodyParser = require('koa-bodyparser');
+const sniffer = require('../index.js');
+const router = require('koa-router')();
+
+router.get('/200', function *() {
+    this.body = 'hi jack'
+});
+
+router.get('/204', function *() {
+    this.status = 204;
+});
+
+router.get('/401', function *() {
+    this.status = 401;
+});
+
+router.get('/500', function *() {
+    this.status = 500;
+});
+
+const snifferConfig = {
+    "apptag" : 'onionsBackend',
+    "api" : 'http://10.8.8.8:4600/api/v3_5/httplog'
+};
+
+app.use(bodyParser());
+app.use(sniffer.logSniffer(snifferConfig));
+app.use(router.routes());
+
+app.listen(3000);
+
+console.log('app started on port 3000');
---
+++ test/httpTest.js
@@ -0,0 +1,48 @@
+'use strict'
+const app = require('./app');
+const request = require('supertest').agent(app.listen());
+const assert = require('assert');
+const mocha = require('mocha');
+const coMocha = require('co-mocha');
+coMocha(mocha);
+
+
+describe('/200', function() {
+    it('GET /200', function *() {
+        let res = yield request
+            .get('/200')
+            .expect(200)
+    })
+});
+
+describe('/204', function() {
+    it('GET /204', function *() {
+        let res = yield request
+            .get('/204')
+            .expect(204)
+    })
+});
+
+describe('/401', function() {
+    it('GET /401', function *() {
+        let res = yield request
+            .get('/401')
+            .expect(401)
+    })
+});
+
+describe('/404', function() {
+    it('GET /404', function *() {
+        let res = yield request
+            .get('/404')
+            .expect(404)
+    })
+});
+
+describe('/500', function() {
+    it('GET /500', function *() {
+        let res = yield request
+            .get('/500')
+            .expect(500)
+    })
+});
