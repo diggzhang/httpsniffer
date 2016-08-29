f2b845613dd97fcfe6a3e0d7846a12311171759a
diggzhang <diggzhang@gmail.com>
2016-08-29 11:46:32 +0800

    Added HEAD method filter

--- index.js
+++ index.js
@@ -16,56 +16,62 @@ module.exports.logSniffer = function (proxy) {
     let api = proxy['api'] || "http://localhost:4600/api/v3_5/httplog";
     return function *logSniffer(next) {
 
-
         let err = null;
+
+        // filter HEAD method requests
         if (this.request.method == 'HEAD') {
             yield *next;
-        }
+        } else {
+            try {
+                yield *next;
+            } catch (e) {
+                err = e;
+                throw err;
+            } finally {
+                let logMsg = {
+                    apptag: apptag,
+                    url: this.request.href,
+                    method: this.request.method,
+                    status: this.status,
+                    request: this.request.body,
+                    response: this.response.body,
+                    ua: this.header['user-agent'],
+                    device: this.header['device'],
+                    eventTime: this.header['eventtime'],
+                };
 
-        try{
-            yield *next;
-        } catch (e) {
-            err = e;
-            throw err;
-        } finally {
-            let logMsg = {
-                apptag: apptag,
-                url: this.request.href,
-                method: this.request.method,
-                status: this.status,
-                request: this.request.body,
-                response: this.response.body,
-                ua: this.header['user-agent'],
-                device: this.header['device'],
-                eventTime: this.header['eventtime'],
-            };
-
-            if (err) {
-                logMsg['response'] = err.message;
-                logMsg['status'] = err.status || 500;
-            }
-
-            if (typeof this.header['remoteip'] != undefined) {
-                logMsg['ip'] = this.header['remoteip'];
-            } else if (this.get('X-Forwarded-For') != '') {
-                let forwardedIpsStr = this.get('X-Forwarded-For');
-                let forwardedIp = forwardedIpsStr.split(',')[0];
-                logMsg['ip'] = forwardedIp;
-            } else {
-                logMsg['ip'] = undefined;
-            }
-
-            if (this.header['authorization'] != undefined) {
-                logMsg['token'] = this.header['authorization'];
-            } else {
-                logMsg['token'] = undefined;
-            }
-
-            request({method:'POST', url: api, body: logMsg, headers: {"connection": "keep-alive"}}, function(error) {
-                if (error) {
-                    console.error("httpSnifferRequest:" + error);
+                if (err) {
+                    logMsg['response'] = err.message;
+                    logMsg['status'] = err.status || 500;
                 }
-            });
+
+                if (typeof this.header['remoteip'] != undefined) {
+                    logMsg['ip'] = this.header['remoteip'];
+                } else if (this.get('X-Forwarded-For') != '') {
+                    let forwardedIpsStr = this.get('X-Forwarded-For');
+                    let forwardedIp = forwardedIpsStr.split(',')[0];
+                    logMsg['ip'] = forwardedIp;
+                } else {
+                    logMsg['ip'] = undefined;
+                }
+
+                if (this.header['authorization'] != undefined) {
+                    logMsg['token'] = this.header['authorization'];
+                } else {
+                    logMsg['token'] = undefined;
+                }
+
+                request({
+                    method: 'POST',
+                    url: api,
+                    body: logMsg,
+                    headers: {"connection": "keep-alive"}
+                }, function (error) {
+                    if (error) {
+                        console.error("httpSnifferRequest:" + error);
+                    }
+                });
+            }
         }
 
     };
