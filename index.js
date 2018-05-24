485ef854002a8aabf9fc7c973c799add267e82ba
diggzhang <diggzhang@gmail.com>
2018-05-24 11:54:11 +0800

    Fallback and add rules

--- index.js
+++ index.js
@@ -1,78 +1,86 @@
 "use strict";
 
+/*
+ * request     - use for request to api
+ */
 const request = require('request').defaults({
-  json: true
+    json: true
 });
 
+/*
+ * http sniffer function
+ */
 module.exports.logSniffer = function (proxy) {
 
-  let apptag = proxy['apptag'] || "defaultBackend";
-  let api = proxy['api'] || "http://localhost:4600/api/v3_5/httplog";
-  return async function logSniffer(ctx, next) {
+    let apptag = proxy['apptag'] || "defaultBackend";
+    let api = proxy['api'] || "http://localhost:4600/api/v3_5/httplog";
+    return function *logSniffer(next) {
 
-    let err = null;
-    if (ctx.request.method == 'HEAD'
-      || /orderProcessor\/users\/query$/.test(ctx.request.href)
-      || /notifications/.test(ctx.request.href)
-      || /cosplay/.test(this.request.href)
-      || /task/.test(this.request.href)
-      || /teacherShows/.test(this.request.href)
-      || /course-tree/.test(this.request.href)
-      || /parents/.test(this.request.href)
-    ) {
-      await next();
-    } else {
-      try {
-        await next();
-      } catch (e) {
-        err = e;
-        throw err;
-      } finally {
-        let logMsg = {
-          apptag: apptag,
-          url: ctx.request.href,
-          method: ctx.request.method,
-          status: ctx.status,
-          request: ctx.request.body,
-          response: ctx.response.body,
-          ua: ctx.header['user-agent'],
-          device: ctx.header['device'],
-          eventTime: ctx.header['eventtime'],
-          apiTime: Date.now()
-        };
-
-        if (err) {
-          logMsg['response'] = err.message;
-          logMsg['status'] = err.status || 500;
-        }
-
-        if (typeof ctx.header['remoteip'] != undefined) {
-          logMsg['ip'] = ctx.header['remoteip'];
-        } else if (ctx.get('X-Forwarded-For') != '') {
-          let forwardedIpsStr = ctx.get('X-Forwarded-For');
-          let forwardedIp = forwardedIpsStr.split(',')[0];
-          logMsg['ip'] = forwardedIp;
+        let err = null;
+        // filter HEAD method requests
+        if (this.request.method == 'HEAD'
+                || /orderProcessor\/users\/query$/.test(this.request.href)
+                || /notifications/.test(this.request.href)
+                || /cosplay/.test(this.request.href)
+                || /task/.test(this.request.href)
+                || /teacherShows/.test(this.request.href)
+                || /course-tree/.test(this.request.href)
+                || /parents/.test(this.request.href)
+        ) {
+            yield *next;
         } else {
-          logMsg['ip'] = undefined;
+            try {
+                yield *next;
+            } catch (e) {
+                err = e;
+                throw err;
+            } finally {
+                let logMsg = {
+                    apptag: apptag,
+                    url: this.request.href,
+                    method: this.request.method,
+                    status: this.status,
+                    request: this.request.body,
+                    response: this.response.body,
+                    ua: this.header['user-agent'],
+                    device: this.header['device'],
+                    eventTime: this.header['eventtime'],
+                    apiTime: Date.now()
+                };
+
+                if (err) {
+                    logMsg['response'] = err.message;
+                    logMsg['status'] = err.status || 500;
+                }
+
+                if (typeof this.header['remoteip'] != undefined) {
+                    logMsg['ip'] = this.header['remoteip'];
+                } else if (this.get('X-Forwarded-For') != '') {
+                    let forwardedIpsStr = this.get('X-Forwarded-For');
+                    let forwardedIp = forwardedIpsStr.split(',')[0];
+                    logMsg['ip'] = forwardedIp;
+                } else {
+                    logMsg['ip'] = undefined;
+                }
+
+                if (this.header['authorization'] != undefined) {
+                    logMsg['token'] = this.header['authorization'];
+                } else {
+                    logMsg['token'] = undefined;
+                }
+
+                request({
+                    method: 'POST',
+                    url: api,
+                    body: logMsg,
+                    headers: {"connection": "keep-alive"}
+                }, function (error) {
+                    if (error) {
+                        console.error("httpSnifferRequest:" + error);
+                    }
+                });
+            }
         }
 
-        if (ctx.header['authorization'] != undefined) {
-          logMsg['token'] = ctx.header['authorization'];
-        } else {
-          logMsg['token'] = undefined;
-        }
-
-        request({
-          method: 'POST',
-          url: api,
-          body: logMsg,
-          headers: {"connection": "keep-alive"}
-        }, function (error) {
-          if (error) {
-            console.error("httpSnifferRequest:" + error);
-          }
-        });
-      }
-    }
-  }
+    };
 };
